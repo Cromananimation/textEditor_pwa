967611449fdb68678ec49548d81fd3a9a78e4494
Cesar A. Roman <130246064+Cromananimation@users.noreply.github.com>
2023-09-23 08:56:29 -0500

    copied over json from class repo

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,7 +26,7 @@ module.exports = () => {
         theme_color: '#31a9e1',
         icons: [
           {
-            src: path.resolve('src/assets/icon.png'),
+            src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           }
